062ba65324ab850f73550d073a4350238182209c
kris <cmuchiri810@gmail.com>
2022-03-21 15:51:53 +0300

    modified html/js

--- script.js
+++ script.js
@@ -11,7 +11,36 @@ function Getpizza(name,size,crust,topping,total){
 $(document).ready(function(){
     $("button.proceed").click(function(){
         $("button.proceed").hide();
-        $("#").hide();
-        $("")
-    })
-})
+        $("#information").hide();
+        $("div.choice").show();
+    });
+});
+
+$("button.proceed").click(function(){
+    let pname = $("#name option:selected").val();
+    let psize = $("#size option:selected").val();
+    let pcrust = $("#crust option:selected").val();
+    let ptopping = [];
+    $.each($("input[name='toppings']:checked"), function(){
+        ptopings.push($(this).val());
+    });
+    console.log(ptoppings.join(", "));
+    switch(psize){
+        case "0":
+          price =0;
+        break;
+        case "large":
+           price = 1200;
+           console.log(price);
+         break;
+         case "medium":
+           price = 850;
+           console.log("The price is "+price);
+         break;
+         case "small":
+           price = 600;
+           console.log(price);
+         default:
+           console.log("error"); 
+       }
+});

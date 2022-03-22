a6386186431e6c2e981f5f987f007690c391c3f3
kris <cmuchiri810@gmail.com>
2022-03-22 15:02:46 +0300

    modified html/css/js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// var crust_price, topping_price;
-// let total = 0;
-// function Getpizza(name,size,crust,topping,total){
-//     this.name = name;
-//     this.size = size;
-//     this.crust = crust;
-//     this.topping = topping;
-//     this.total = total;
-// }
+// Business Logic
+function Order(name,size,crust,topping,total){
+  this.name = name;
+  this.size = size;
+  this.crust= crust;
+  this.topping= topping;
+  this.total = total;
+}
+
 
 // User Interface
 $(document).ready(function(){
@@ -21,51 +21,39 @@ $(document).ready(function(){
       let crustPrice= parseInt($("#crust option:selected").val());
       let toppingPrice= parseInt($("#topping option:selected").val());
       let total = sizePrice + crustPrice + toppingPrice;
-      console.log(total);
+      let finalPrice = total;
 
 
       $("#pizzaname").html(pizzaName);
       $("#pizzasize").html(pizzasize);
       $("#pizzacrust").html(pizzacrust);
       $("#pizzatopping").html(pizzatopping);
-      $("#totals").html(total);
+      $("#totals").html(finalPrice);
+
+      $(".add").click(function() {
+        let pizzaName= $("#name option:selected").text();
+        let pizzasize= $("#size option:selected").text();
+        let pizzacrust= $("#crust option:selected").text();
+        let pizzatopping= $("#topping option:selected").text();
+      
+
+        let sizePrice= parseInt($("#size option:selected").val());
+        let crustPrice= parseInt($("#crust option:selected").val());
+        let toppingPrice= parseInt($("#topping option:selected").val());
+        let total = sizePrice + crustPrice + toppingPrice;
+        let finalPrice = total;
+
+        let newOrder = new Order(pizzaName, pizzasize, pizzacrust, pizzatopping, total);
+
+        let addQuantity = '<tr>' + '<td id="pizzaname">' + newOrder.name + '</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">' + newOrder.crust + '</td><td id="pizzatopping">' + newOrder.topping +  '</td><td id="totals">' + newOrder.total + '</td></tr>'
+
+        $("#ordersmade").append(addQuantity);  
+
+        console.log(finalPrice);
 
 
-      function order(name,size,crust,topping,total){
-        this.name = name;
-        this.size = size;
-        this.crust= crust;
-        this.topping= topping;
-        this.total = total;
-      }
+      });
+
     });
 });
 
-// $("button.proceed").click(function(){
-//     let pname = $("#name option:selected").val();
-//     let psize = $("#size option:selected").val();
-//     let pcrust = $("#crust option:selected").val();
-//     let ptopping = [];
-//     $.each($("input[name='toppings']:checked"), function(){
-//         ptopings.push($(this).val());
-//     });
-//     console.log(ptoppings.join(", "));
-//     switch(psize){
-//         case "0":
-//           price =0;
-//         break;
-//         case "large":
-//            price = 1200;
-//            console.log(price);
-//          break;
-//          case "medium":
-//            price = 850;
-//            console.log("The price is "+price);
-//          break;
-//          case "small":
-//            price = 600;
-//            console.log(price);
-//          default:
-//            console.log("error"); 
-//        }
-// });
